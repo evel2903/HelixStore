a422f0c08f7fc1c2a44fbba12c796192845a3a64
evel2903 <evel.lxs.035@gmail.com>
2021-05-15 00:46:21 +0700

    create Winform UI

---
+++ HelixStore.WinForm/Forms/StaffForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace HelixStore.WinForm.Forms
+{
+    partial class StaffForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Staff_Layout = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Staff_Layout.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Staff_Layout
+            // 
+            this.Staff_Layout.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(249)))), ((int)(((byte)(252)))));
+            this.Staff_Layout.Controls.Add(this.label1);
+            this.Staff_Layout.Location = new System.Drawing.Point(0, 0);
+            this.Staff_Layout.Margin = new System.Windows.Forms.Padding(0);
+            this.Staff_Layout.Name = "Staff_Layout";
+            this.Staff_Layout.Size = new System.Drawing.Size(952, 593);
+            this.Staff_Layout.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(155, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "staff";
+            // 
+            // StaffForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1225, 753);
+            this.Controls.Add(this.Staff_Layout);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "StaffForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "StaffForm";
+            this.Staff_Layout.ResumeLayout(false);
+            this.Staff_Layout.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Staff_Layout;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ HelixStore.WinForm/Index.cs
@@ -0,0 +1,118 @@
+﻿
+using HelixStore.WinForm.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelixStore.WinForm
+{
+    public partial class Index : Form
+    {
+        private Form currentLayoutForm;
+
+        public Index()
+        {
+            InitializeComponent();
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
+        }
+
+        private void btn_home_Click(object sender, EventArgs e)
+        {
+            ActiveButton(btn_home);
+            RenderMainLayout(new HomeForm());
+            
+        }
+
+        private void btn_products_Click(object sender, EventArgs e)
+        {
+            ActiveButton(btn_products);
+            RenderMainLayout(new ProductForm());
+        }
+
+        private void btn_staffs_Click(object sender, EventArgs e)
+        {
+            ActiveButton(btn_staffs);
+            RenderMainLayout(new StaffForm());
+        }
+
+        private void btn_invoice_Click(object sender, EventArgs e)
+        {
+            ActiveButton(btn_invoice);
+            RenderMainLayout(new InvoiceForm());
+        }
+
+        private void btn_customer_Click(object sender, EventArgs e)
+        {
+            ActiveButton(btn_customer);
+            RenderMainLayout(new CustomersForm());
+        }
+
+        private void btn_logout_Click(object sender, EventArgs e)
+        {
+            ActiveButton(btn_logout);
+        }
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+        private void ActiveButton(Button btn)
+        {
+            //Reset to default
+            btn_home.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(250)))), ((int)(((byte)(253)))));
+            btn_products.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(250)))), ((int)(((byte)(253)))));
+            btn_staffs.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(250)))), ((int)(((byte)(253)))));
+            btn_invoice.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(250)))), ((int)(((byte)(253)))));
+            btn_logout.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(250)))), ((int)(((byte)(253)))));
+            btn_customer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(250)))), ((int)(((byte)(253)))));
+            //Active button Clicked
+            btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(241)))), ((int)(((byte)(247)))));
+        }
+
+        private void RenderMainLayout(Form form)
+        {
+           
+
+            if (currentLayoutForm != null)
+            {
+                currentLayoutForm.Close();
+                Main_Content.Controls.Clear();
+            }
+
+            currentLayoutForm = form;
+            //End
+            form.TopLevel = false;
+            form.FormBorderStyle = FormBorderStyle.None;
+            form.Location = new System.Drawing.Point(6, 6);
+            form.Padding = new System.Windows.Forms.Padding(0, 0, 0, 0);
+            form.Margin = new System.Windows.Forms.Padding(0);
+            Main_Content.Controls.Add(form);
+            Main_Content.Tag = form;
+            form.BringToFront();
+            form.Show();
+
+        }
+
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,     // x-coordinate of upper-left corner
+            int nTopRect,      // y-coordinate of upper-left corner
+            int nRightRect,    // x-coordinate of lower-right corner
+            int nBottomRect,   // y-coordinate of lower-right corner
+            int nWidthEllipse, // width of ellipse
+            int nHeightEllipse // height of ellipse
+        );
+
+
+    }
+
+}
